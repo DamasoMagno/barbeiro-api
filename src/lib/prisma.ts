414c35f0d0ff8454de91cea7e8c9f7105e8d1142
DamasoMagno <limamdamaso@gmail.com>
2025-12-07 11:35:20 -0300

    feat: added pg to allow connection in db

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,8 +2,12 @@ import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../generated/prisma/client";
 import { env } from "../env";
+import pg from "pg";
 
-const adapter = new PrismaPg({ url: env.DATABASE_URL });
+const pool = new pg.Pool({
+  connectionString: env.DATABASE_URL,
+});
+const adapter = new PrismaPg(pool);
 const prisma = new PrismaClient({ adapter });
 
 export { prisma };
